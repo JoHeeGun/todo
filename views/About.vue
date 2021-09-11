6ac123a7ed3c246d2151f564467ecd5a1505501a
JOHEEGUN <whgmlrjs123@naver.com>
2021-09-11 19:02:21 +0900

    create todoapp project

---
+++ views/About.vue
@@ -0,0 +1,9 @@
+<template>
+  <section class="about">
+    <div>
+      <h2>Jo Hee Gun</h2>
+      <h2>whgmlrjs123@naver.com</h2>
+      <h2>010-3940-6688</h2>
+    </div>
+  </section>
+</template>
